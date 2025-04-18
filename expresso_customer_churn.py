22e7033b5dbc8376532359ae647340966a099953
TaibatNofiu <147071304+TaibatNofiu@users.noreply.github.com>
2025-04-18 10:52:10 +0100

    Update expresso_customer_churn.py

--- expresso_customer_churn.py
+++ expresso_customer_churn.py
@@ -13,7 +13,7 @@ st.write('This model use LogisticRegression to make prediction')
 st.sidebar.header("Input features for prediction")
 
 # Upload the dataset
-uploaded_file = st.file_uploader("C:\Users\AMOKE\Desktop\GMC\ML\Expresso_churn_dataset.zip")
+uploaded_file = st.file_uploader("Desktop\GMC\ML\Expresso_churn_dataset.zip")
 # Load the dataset
 data = pd.read_csv(uploaded_file, compression='zip')
 expresso_df = data.drop(columns = ['user_id', 'MONTANT', 'DATA_VOLUME', 'ARPU_SEGMENT', 'ORANGE', 'TIGO', 'ZONE1', 'ZONE2'])
