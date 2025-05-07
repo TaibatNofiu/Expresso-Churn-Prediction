fe9339fd6aa190656ee15d4c3f1134e7d13c8650
TaibatNofiu <147071304+TaibatNofiu@users.noreply.github.com>
2025-05-07 12:16:29 +0100

    Update expresso_customer_churn.py

--- expresso_customer_churn.py
+++ expresso_customer_churn.py
@@ -79,7 +79,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 @st.cache_resource
 def train_model(X, y):
     # Instantiate SMOTE
-    smote = SMOTE(k_neighbors = 3, n_jobs = -1)
+    smote = SMOTE(k_neighbors = 3, random_state = 42)
     # Fit and resample X_train, y_train
     X_smote, y_smote = smote.fit_resample(X_train, y_train)
     # Instantiate LogisticRegression
