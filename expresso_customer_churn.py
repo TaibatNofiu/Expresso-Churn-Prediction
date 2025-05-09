06e7a0a92c1b85726e535fc86a3dfe0ac561821e
TaibatNofiu <147071304+TaibatNofiu@users.noreply.github.com>
2025-05-09 18:30:30 +0100

    Update expresso_customer_churn.py

--- expresso_customer_churn.py
+++ expresso_customer_churn.py
@@ -1,106 +1,62 @@
 #### Import the libraries
 import pandas as pd
 import streamlit as st
-import numpy as np
-import gdown
-from sklearn.linear_model import LogisticRegression
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import train_test_split
+import joblib
 
 st.title('Expresso Customers Churn Prediction')
 st.write('This model use LogisticRegression to make prediction')
 
+# download model
+model = joblib.load("https://github.com/TaibatNofiu/Expresso-Churn-Prediction/main/expressomodel.pkl")
+features = joblib.load("https://github.com/TaibatNofiu/Expresso-Churn-Prediction/main/features.pkl")
+
 # Create user input (widgets)
-st.sidebar.header("Input features for prediction")
-
-# Load the dataset
-@st.cache_data
-def load_data():
-    url = "https://drive.google.com/uc?id=1h_dzCN7rbOspMJcM-N_GyPhJWpLxsAZr"
-    output = "expresso_dataset.csv"
-    gdown.download(url, output, quiet=False)
-    data = pd.read_csv(output)
-    data.columns = data.columns.str.strip()
-    return data
-data = load_data()
-expresso_df = data.drop(columns = ['user_id', 'MONTANT', 'DATA_VOLUME', 'ARPU_SEGMENT', 'ORANGE', 'TIGO', 'ZONE1', 'ZONE2'])
-expresso_df = expresso_df.dropna(thresh=int(0.7 * expresso_df.shape[1]), axis=0)
-expresso_df['REGION'] = expresso_df['REGION'].fillna(expresso_df['REGION'].mode()[0])
-expresso_df['FREQUENCE_RECH'] = expresso_df['FREQUENCE_RECH'].fillna(expresso_df['FREQUENCE_RECH'].mean())
-expresso_df.dropna(inplace = True, subset = ['REVENUE'])
-expresso_df.dropna(inplace = True, subset = ['FREQUENCE'])
-expresso_df['ON_NET'] = expresso_df['ON_NET'].fillna(expresso_df['ON_NET'].mean())
-expresso_df['TOP_PACK'] = expresso_df['TOP_PACK'].fillna(expresso_df['TOP_PACK'].mode()[0])
-expresso_df['FREQ_TOP_PACK'] = expresso_df['FREQ_TOP_PACK'].fillna(expresso_df['FREQ_TOP_PACK'].median())
-expresso_df['Churn_name'] = np.where(expresso_df['CHURN'] == 0, 'No', 'Yes')
-
-# Display the dataset
-st.write('Expresso Customers Churn', expresso_df.head(50))
+st.header("Input features for prediction")
 
 # The features
-region = st.sidebar.selectbox('Region', expresso_df['REGION'].unique())
-tenure = st.sidebar.selectbox('Tenure', expresso_df['TENURE'].unique())
-freq_rech = st.sidebar.slider('Frequence Reach',
-                              float(expresso_df['FREQUENCE_RECH'].min()),
-                              float(expresso_df['FREQUENCE_RECH'].mean()),
-                              float(expresso_df['FREQUENCE_RECH'].max()))
-revenue = st.sidebar.number_input('Enter revenue', min_value = 0)
-freq = st.sidebar.slider('Frequence',
-                        float(expresso_df['FREQUENCE'].min()),
-                        float(expresso_df['FREQUENCE'].mean()),
-                        float(expresso_df['FREQUENCE'].max()))
-on_net = st.sidebar.slider('ON_NET',
-                            float(expresso_df['ON_NET'].min()),
-                            float(expresso_df['ON_NET'].mean()),
-                            float(expresso_df['ON_NET'].max()))
-regular = st.sidebar.slider('Regularity',
-                              float(expresso_df['REGULARITY'].min()),
-                              float(expresso_df['REGULARITY'].mean()),
-                              float(expresso_df['REGULARITY'].max()))
-freq_top_pack = st.sidebar.slider('Freq Top Pack',
-                              float(expresso_df['FREQ_TOP_PACK'].min()),
-                              float(expresso_df['FREQ_TOP_PACK'].mean()),
-                              float(expresso_df['FREQ_TOP_PACK'].max()))
-top_pack = st.sidebar.selectbox('Top Pack', expresso_df['TOP_PACK'].unique())
-# Select the columns to be used for modeling
-express_df = expresso_df[['REGION', 'TENURE', 'FREQUENCE_RECH', 'REVENUE', 'FREQUENCE', 'ON_NET', 'REGULARITY', 'FREQ_TOP_PACK', 'TOP_PACK', 'CHURN']]
-# Encode the categorical features
-encode_data = pd.get_dummies(express_df, columns = express_df.select_dtypes(include = 'object').columns)
-
-# split the data into X and y and also into train and test
-# Features (X) and target(y)
-X = encode_data.drop(columns = 'CHURN')
-y = encode_data['CHURN']
-
-# Train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-
-# Fit smote data
-@st.cache_resource
-def train_model(X, y):
-    # Instantiate SMOTE
-    smote = SMOTE(k_neighbors = 3, random_state = 42)
-    # Fit and resample X_train, y_train
-    X_smote, y_smote = smote.fit_resample(X_train, y_train)
-    # Instantiate LogisticRegression
-    logreg = LogisticRegression()
-    logreg.fit(X_smote, y_smote)
-    return logreg
-model = train_model(X_train, y_train)
+region = st.selectbox('Region', ['FATICK', 'DAKAR', 'LOUGA', 'TAMBACOUNDA', 'KAOLACK', 'THIES',
+       'SAINT-LOUIS', 'KAFFRINE', 'DIOURBEL', 'MATAM', 'KOLDA',
+       'ZIGUINCHOR', 'SEDHIOU', 'KEDOUGOU'])
+tenure = st.selectbox('Tenure', ['K > 24 month', 'I 18-21 month', 'H 15-18 month', 'J 21-24 month',
+       'F 9-12 month', 'G 12-15 month', 'E 6-9 month', 'D 3-6 month'])
+freq_rech = st.number_input('Frequence Reach', min_value = 1, max_value = 150, value = 15)
+revenue = st.number_input('Enter revenue', min_value = 1, max_value = 550000, value = 20000)
+freq = st.slider('Frequence', 1, 100)
+on_net = st.number_input('ON_NET', min_value = 10, max_value = 60000, value = 20)
+regular = st.number_input('Regularity', min_value = 1, max_value = 70, value = 12)
+freq_top_pack = st.number_input('Freq Top Pack', min_value = 1, max_value = 750, value = 15)
+MRG = st.selectbox('MRG', ['NO'])
 
 # Prepare a dataframe
 user_input = pd.DataFrame({
-    'REGION': [region],
-    'TENURE': [tenure],
     'FREQUENCE_RECH': [freq_rech],
+    'REGION': [region],
     'REVENUE': [revenue],
     'FREQUENCE': [freq],
     'ON_NET': [on_net],
     'REGULARITY': [regular],
     'FREQ_TOP_PACK': [freq_top_pack],
-    'TOP_PACK': top_pack  # or allow user to choose
+    'TENURE': [tenure],
+    'MRG': [MRG]
 })
 
+# Display input data
+st.subheader('Input')
+st.write(user_input)
+
+# Encode user_input to match training data
+user_input_encoded = pd.get_dummies(user_input, columns = user_input.select_dtypes(include = 'object').columns)
+user_input_encoded = user_input_encoded.reindex(columns= features, fill_value=0)
+
+# Predict user_input
+if st.button('Predict'):
+    prediction = model.predict(user_input_encoded)
+    pred_prob = model.predict_proba(user_input_encoded)
+
+    if prediction[0] == 1:
+        st.success(f'The probability that the customer will leave is {pred_prob[0][1] * 100:.2f}%')
+    else:
+        st.success(f'The probability that the customer will stay {pred_prob[0][0] * 100:.2f}%')
 # Encode user_input to match training data
 user_input_encoded = pd.get_dummies(user_input, columns = user_input.select_dtypes(include = 'object').columns)
 user_input_encoded = user_input_encoded.reindex(columns = X.columns, fill_value = 0)
